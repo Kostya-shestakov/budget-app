e985d6a9300ec2e8491b280d92d78bc192428d25
kostya-dev <bezdusnyjkosta3@gmail.com>
2021-10-14 18:49:14 +0500

    Updates

---
+++ precache-manifest.0cbf007566b38b3f19d8e9821f526fb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b74c18383b2ba59a728b48c033901e85",
+    "url": "/budget-app/index.html"
+  },
+  {
+    "revision": "27c1a2021ed49ba256fb",
+    "url": "/budget-app/static/css/main.d60c3e63.chunk.css"
+  },
+  {
+    "revision": "f1b649579c500c0f1837",
+    "url": "/budget-app/static/js/2.0c241bc6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/budget-app/static/js/2.0c241bc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27c1a2021ed49ba256fb",
+    "url": "/budget-app/static/js/main.abdfd6bf.chunk.js"
+  },
+  {
+    "revision": "ddef51c8f7043137a866",
+    "url": "/budget-app/static/js/runtime-main.48d5e94f.js"
+  }
+]);
